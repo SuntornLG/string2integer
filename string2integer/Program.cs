70e00cbd029866f70db065429444a7fc892c857b
SuntornLG <laktongsuntorn@gmail.com>
2020-05-11 20:06:10 +0700

    first committed

---
+++ string2integer/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace string2integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string response = "No digit found";
+            var result = GetIntFromString("1e0we0wew33");
+            if (result.HasValue)
+                response = string.Format("Found digit : {0}", result);
+
+            Console.WriteLine(response);
+        }
+
+        static int? GetIntFromString(string param)
+        {
+            // ASII 48 - 57 for digit number.
+            byte[] asciiBytes = System.Text.Encoding.ASCII.GetBytes(param);
+            int? resultNum = null;
+            foreach (var n in asciiBytes)
+            {
+                var num = GetNumber(n);
+                if (num != -1)
+                {
+                    resultNum = Concat(resultNum, num);
+                }
+            }
+            return resultNum;
+        }
+
+        static int Concat(int? a, int b)
+        {
+
+            int pow = 1;
+            if (b > 0)
+            {
+                while (pow < b)
+                {
+                    pow = ((pow << 2) + pow) << 1;
+                    a = ((a << 2) + a) << 1;
+                }
+            }
+            else
+            {
+                a = ((a << 2) + a) << 1;
+            }
+
+            if (a != null)
+                return a.Value + b;
+            else
+                return b;
+        }
+
+        static int GetNumber(byte asiiCode)
+        {
+            switch (asiiCode)
+            {
+                case 48:
+                    return 0;
+                case 49:
+                    return 1;
+                case 50:
+                    return 2;
+                case 51:
+                    return 3;
+                case 52:
+                    return 4;
+                case 53:
+                    return 5;
+                case 54:
+                    return 6;
+                case 55:
+                    return 7;
+                case 56:
+                    return 8;
+                case 57:
+                    return 9;
+            }
+            return -1;
+        }
+    }
+}
